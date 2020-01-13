5031b8a852e8ccdb84ef1b120f7530a6d8b3a8f1
PySom <nwisuchisom@gmail.com>
2020-01-13 14:40:59 +0100

    adding build file for studymate

---
+++ precache-manifest.fc533bb8aff69f5f6ca80541f337f833.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232e7e147c28c0f7a06945be97328bb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68ca2e32f91e52eb5612",
+    "url": "./static/css/2.54fd521d.chunk.css"
+  },
+  {
+    "revision": "12c61448aba292b3c8e9",
+    "url": "./static/css/main.f8b11650.chunk.css"
+  },
+  {
+    "revision": "68ca2e32f91e52eb5612",
+    "url": "./static/js/2.5a1cb464.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.5a1cb464.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c61448aba292b3c8e9",
+    "url": "./static/js/main.eb169f06.chunk.js"
+  },
+  {
+    "revision": "c9f437e9e1652ef9e6a6",
+    "url": "./static/js/runtime-main.4d647a16.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e88f08cd2235076b39a95df0b8614c88",
+    "url": "./static/media/register-background.e88f08cd.png"
+  }
+]);
